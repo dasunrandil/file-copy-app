42c844923f1f887ec07c0dc830f1a81d00399327
dasunrandil <dasunrandil@gmail.com>
2022-10-13 11:42:21 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# File Copy Application
+
+### Version
+0.0.0
+
+### License
+
+Copyright © 20222 JELLY. All Rights Reserved. 
+This project is licensed under the [MIT](LICENSE.txt) license. 
+
+
+
+
---
+++ src/main/java/controller/MainFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+
+public class MainFormController {
+    public JFXButton btnSelectFile;
+    public JFXButton btnSelectDirectory;
+    public Rectangle pgbContainer;
+    public Rectangle gbBar;
+    public JFXButton btnCopy;
+    public Label lblTitle;
+
+    public void btnSelectFileOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnSelectDirectoryOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnCopyOnAction(ActionEvent actionEvent) {
+    }
+}
